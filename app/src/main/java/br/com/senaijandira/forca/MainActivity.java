2ed06409d0301d181331060ee7bac3606ad3c6a4
DAVID BITENCOURT <17259198@CFP127ALUNO.LOCAL>
2018-09-10 11:07:03 -0300

    Segunda commit

--- app/src/main/java/br/com/senaijandira/forca/MainActivity.java
+++ app/src/main/java/br/com/senaijandira/forca/MainActivity.java
@@ -46,46 +46,33 @@ public class MainActivity extends AppCompatActivity {
 
     StringBuilder palavraEscondida = new StringBuilder();
 
-
-    // Método que inicia o jogo
     public void iniciarJogo() {
 
 
-        // Criando um Random
         random = new Random();
 
-        // qtdDicas recebe o tamanho do vetor
         qtdDicas = dica.length;
 
-        // indiceDica recebe um indice sorteado do vetor dicas
+
         indiceDica = random.nextInt(qtdDicas);
 
-        // qtdPalavras recebe o tamanho das colunas da matriz
         qtdPalavras = palavra[0].length;
 
-        // indiceSorteio recebe um indice sorteado do vetor
         sortearPalavra = random.nextInt(qtdPalavras);
 
-        // palavraSorteada recebe a palavra referente a linha com indiceDica, coluna com indiceSorteio e com todas as letras maiúsculas
         palavraSorteada = palavra[indiceDica][sortearPalavra].toUpperCase();
 
-        // Loop que conta até o tamanho da palavra sorteada
         for (int i = 0; i < palavraSorteada.length(); i++) {
 
-            // A cada volta do loop, acrescenta um "_ " na palavraEscondida através do método append que concatena strings
             palavraEscondida.append("_ ");
 
         }
 
-        // Exibindo na tela as letras escondidas da palavra
+
         txtPalavra.setText(palavraEscondida.toString());
 
-
-        // Exibindo a quantidade de letras na palavra
         txtLetras.setText(palavraSorteada.length() + " letras");
 
-
-        //iniciar a música
         mediaPlayer = MediaPlayer.create(this, R.raw.super_mario_world);
         mediaPlayer.start();
 
@@ -94,34 +81,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // Método que é chamado com o clique dos botões de A-Z
     public void pegarLetra(View view) {
 
-        // botao recebe a view que foi clicada na tela
         Button botao = (Button) view;
 
-        // letra recebe o texto que está no botão clicado
         String letra = botao.getText().toString();
 
-
-        // compararAcertos recebe acertos
         compararAcertos = acertos;
 
-        // Loop que conta até o tamanho da palavra sorteada
         for (int i = 0; i < palavraSorteada.length(); i++) {
 
-            // Verefica se tem a letra clicada na palavra
             if (letra.equals(String.valueOf(palavraSorteada.charAt(i)))) {
 
-                // Se tiver, troca o "_" pela letra digitada e acrescenta 1 nos acertos
-                // i vai ser sempre * 2 para ignorar os espaços em palavraEscondida ao fazer a contagem
                 palavraEscondida.setCharAt(i * 2, letra.charAt(0));
                 acertos += 1;
 
             }
         }
 
-        // Exibe a palavra escondida que vai atualizando
         txtPalavra.setText(palavraEscondida.toString());
 
 
@@ -138,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         txtPalavra = findViewById(R.id.txtPalavra);
         txtLetras = findViewById(R.id.txtLetras);
         txtDicas = findViewById(R.id.txtDicas);
-
+/*
         btnA = findViewById(R.id.btnA);
         btnB = findViewById(R.id.btnB);
         btnC = findViewById(R.id.btnC);
@@ -164,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
         btnW = findViewById(R.id.btnW);
         btnX = findViewById(R.id.btnX);
         btnY = findViewById(R.id.btnY);
-        btnZ = findViewById(R.id.btnZ);
+        btnZ = findViewById(R.id.btnZ);*/
 
         iniciarJogo();
 
